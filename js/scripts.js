b6b19de6897d88b81f9327e687d0d31999604671
edgardo88 <109301607+edgardo88@users.noreply.github.com>
2022-08-05 14:18:55 -0400

    started new with pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
-alert('Hello world');
-
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
-
+let pokemonList = [
+	{	name: 'Venusaur', 
+		height: 2,
+		weight: 100, 
+		types: ['grass' , 'poison']},
+	
+	{	name: 'Butterfree',
+		height: 1.1,
+		weight: 32,
+		types: ['bug' , 'flying']},
+	
+	{	name: 'Jigglypuff',
+		height: 0.5,
+		weight: 5.5,
+		types: ['fairy' , 'normal']},
+];
