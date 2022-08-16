d50d571a1a21f06cb567dcb85617971f1970ecda
edgardo88 <109301607+edgardo88@users.noreply.github.com>
2022-08-16 11:40:44 -0400

    added pokemon respository and wrapped in iife
    added 2 new functions with 1 return

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+let pokemonRepository = (function () {
 let pokemonList = [
 	{	name: 'Venusaur', 
 		height: 2,
@@ -15,6 +16,20 @@ let pokemonList = [
 		types: ['fairy' , 'normal']},
 ];
 
+function getAll() {
+	return pokemonList;
+}
+
+function add(pokemon) {
+	pokemonList.push(pokemon);
+}
+
+return {
+	getAll: getAll,
+	add: add
+}
+})();
+
 // loop that iterates for Pokemons name and weight
 
 for (let i = 0; i < pokemonList.length; i++) { //conditionals for weight
@@ -25,3 +40,14 @@ for (let i = 0; i < pokemonList.length; i++) { //conditionals for weight
 	} else	if (pokemonList[i].weight < 20) { //if Pokemon weighs under 20 it states awe thats tiny
 		document.write('<br>' + pokemonList[i].name + ' (weight: ' + pokemonList[i].weight + ') - small')
 	}}
+
+	let pokemonList = pokemonRepository.getAll();
+
+pokemonList.forEach(function(pokemon) {
+    if (pokemon.height>1) {
+        document.write('<p>' + pokemon.name + ', height: ' + pokemon.height + ' - Wow, that\'s big!' + '</p>')
+    } else {
+        document.write('<p>' + pokemon.name + ', height: ' + pokemon.height + '</p>')
+
+    }
+});
