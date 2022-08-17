e40731406f44330c31f81b096eadd4f20ed49f8f
edgardo88 <109301607+edgardo88@users.noreply.github.com>
2022-08-17 10:19:07 -0400

    Turned off for() and replaced with foreach()

--- js/scripts.js
+++ js/scripts.js
@@ -32,14 +32,14 @@ return {
 
 // loop that iterates for Pokemons name and weight
 
-for (let i = 0; i < pokemonList.length; i++) { //conditionals for weight
-	if (pokemonList[i].weight > 40) { //If Pokemon weighs over 40 it states wow thats big
-		document.write('<br>' + pokemonList[i].name + ' (weight: ' + pokemonList[i].weight + ') - Wow, that\'s big!')
-	} else if (pokemonList[i].weight <= 40 && pokemonList[i].weight > 20) { //If Pokemon is between 20 and 40 it states thats a decent size
-		document.write('<br>' + pokemonList[i].name + ' (weight: ' + pokemonList[i].weight + ') - intresting decent size.')
-	} else	if (pokemonList[i].weight < 20) { //if Pokemon weighs under 20 it states awe thats tiny
-		document.write('<br>' + pokemonList[i].name + ' (weight: ' + pokemonList[i].weight + ') - small')
-	}}
+//for (let i = 0; i < pokemonList.length; i++) { //conditionals for weight
+	//if (pokemonList[i].weight > 40) { //If Pokemon weighs over 40 it states wow thats big
+		//document.write('<br>' + pokemonList[i].name + ' (weight: ' + pokemonList[i].weight + ') - Wow, that\'s big!')
+	//} else if (pokemonList[i].weight <= 40 && pokemonList[i].weight > 20) { //If Pokemon is between 20 and 40 it states thats a decent size
+	//	document.write('<br>' + pokemonList[i].name + ' (weight: ' + pokemonList[i].weight + ') - intresting decent size.')
+	//} else	if (pokemonList[i].weight < 20) { //if Pokemon weighs under 20 it states awe thats tiny
+		//document.write('<br>' + pokemonList[i].name + ' (weight: ' + pokemonList[i].weight + ') - small')
+//	}}
 
 	let pokemonList = pokemonRepository.getAll();
 
