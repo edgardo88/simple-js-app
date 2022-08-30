d80516de7b0056efb7f17734c57461cbb2fbe1d9
edgardo88 <109301607+edgardo88@users.noreply.github.com>
2022-08-30 13:37:26 -0400

    Added list of poke with new functions(load details and showdetails and etc.

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,80 @@
 let pokemonRepository = (function () {
-let pokemonList = [
-	{	name: 'Venusaur', 
-		height: 2,
-		weight: 100, 
-		types: ['grass' , 'poison']},
-	
-	{	name: 'Butterfree',
-		height: 1.1,
-		weight: 32,
-		types: ['bug' , 'flying']},
-	
-	{	name: 'Jigglypuff',
-		height: 0.5,
-		weight: 5.5,
-		types: ['fairy' , 'normal']},
-];
+    let pokemonList = [];
+    let apiUrl ='https://pokeapi.co/api/v2/pokemon/?limit=151';
 
-let pokemonListCharacter= document.querySelector('.pokemon-list');
+    let pokemonListElement = document.querySelector('.pokemon-list');
 
-function getAll() {
-	return pokemonList;
-}
+    function getAll() {
+        return pokemonList;
+    }
 
-function add(pokemon) {
-	pokemonList.push(pokemon);
-}
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
 
-function addListItem(pokemon) {
-	//creating  list item (pokemons) with  button
-	let listItem = document.createElement('li');
-	let button = document.createElement('button');
-	button.innerText = pokemon.name;
-	button.classList.add('pokemon-button');
-	listItem.appendChild(button);
-	pokemonListCharacter.appendChild(listItem);
-	// listens to clicks on pokemon button for pokemon details
-	button.addEventListener('click', function(event){
-		showDetails(pokemon)
-	})
-}; 
+    function addListItem(pokemon) {
+        //creating a list item (pokemons) with a button
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('pokemon-button');
+        // add button to list item and add item(pokemon) to the pokemon list elements in index.html
+        listItem.appendChild(button);
+        pokemonListElement.appendChild(listItem);
+        // listens to clicks on pokemon button to show more details
+        button.addEventListener('click', function(event) {
+            showDetails(pokemon)
+        })
+    }; 
 
-function showDetails(pokemon) {
-	console.log(pokemon.name);
-};
+    //adding Load list function for task
+    function loadList() {
+        return fetch(apiUrl).then(function(response) {
+            return response.json();
+        }).then(function(json) {
+            json.results.forEach(function(item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+                console.log(pokemon);
+            }); 
+        }).catch(function(e) {
+            console.error(e);
+        })  
+    }
 
-return {
-	getAll: getAll,
-	add: add,
-	addListItem: addListItem,
-}
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function(response) {
+            return response.json();
+        }).then(function(details) {
+            item.height = details.height;
+            item.types = details.types;
+            item.imageUrl = details.sprites.front_default;
+        }).catch(function(e) {
+            console.error(e);
+        });
+    }
+
+    function showDetails(pokemon) {
+    loadDetails(pokemon).then(function() {
+     console.log(pokemon);
+        });
+    };
+
+    return {
+        getAll: getAll,
+        add: add,
+        addListItem: addListItem,
+        loadList: loadList,
+        loadDetails: loadDetails,
+    }
 })();
 
-// loop that iterates for Pokemons name and weight
-
-//for (let i = 0; i < pokemonList.length; i++) { //conditionals for weight
-	//if (pokemonList[i].weight > 40) { //If Pokemon weighs over 40 it states wow thats big
-		//document.write('<br>' + pokemonList[i].name + ' (weight: ' + pokemonList[i].weight + ') - Wow, that\'s big!')
-	//} else if (pokemonList[i].weight <= 40 && pokemonList[i].weight > 20) { //If Pokemon is between 20 and 40 it states thats a decent size
-	//	document.write('<br>' + pokemonList[i].name + ' (weight: ' + pokemonList[i].weight + ') - intresting decent size.')
-	//} else	if (pokemonList[i].weight < 20) { //if Pokemon weighs under 20 it states awe thats tiny
-		//document.write('<br>' + pokemonList[i].name + ' (weight: ' + pokemonList[i].weight + ') - small')
-//	}}
-
-	let pokemonList = pokemonRepository.getAll();
-
-pokemonList.forEach(function(pokemon) {
-    pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+    pokemonRepository.getAll().forEach(function(pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    });
 });
